258949259bb32c8a070113f6b96d1c44dada1fc2
a5ea5em5int <ayeayemyint1073@gmail.com>
2024-01-23 21:57:03 +0630

    feature branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,10 @@ package org.example;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public void add(int a, int b)
+    {
+        System.out.println(a+b);
+    }
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
